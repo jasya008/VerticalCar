a2a044aaa40d756298876208ea0b1bb828e93e5e
jasya008 <jasmineabakumova1@gmail.com>
2026-03-14 02:01:52 +0500

    Fully re created software for ESP32

--- VerticalCar.ino
+++ VerticalCar.ino
@@ -1,42 +1,40 @@
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
 #include <Wire.h>
 
-// Пины для моторов
-const int IN1 = PA0; 
-const int IN2 = PA1;
-const int IN3 = PA2;
-const int IN4 = PA3;
-const int ENA = PA6; // Пин для регулировки скорости левого мотора
-const int ENB = PA7; // Пин для регулировки скорости правого мотора
+const int IN1 = 0; const int IN2 = 1;
+const int IN3 = 2; const int IN4 = 3;
+const int ENA = 4; const int ENB = 5;
 
-// РЕГУЛИРОВКА СКОРОСТИ (напряжения)
-// 0 - стоим, 255 - максимум (около 7-8 вольт от аккумуляторов)
-int motorSpeed = 1000; 
+Adafruit_MPU6050 mpu;
 
 void setup() {
-  // Настраиваем все пины на выход
-  pinMode(IN1, OUTPUT);
-  pinMode(IN2, OUTPUT);
-  pinMode(IN3, OUTPUT);
-  pinMode(IN4, OUTPUT);
-  pinMode(ENA, OUTPUT);
-  pinMode(ENB, OUTPUT);
+  Serial.begin(115200);
+  pinMode(IN1, OUTPUT); pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT); pinMode(IN4, OUTPUT);
+  pinMode(ENA, OUTPUT); pinMode(ENB, OUTPUT);
 
-  // Запускаем моторы один раз при старте
-  moveForward(motorSpeed);
+  Serial.println("--- START TEST ---");
+  
+  Wire.begin(8, 9);
+  // Пробуем запустить гироскоп, но не ждем его вечно
+  if (!mpu.begin()) {
+    Serial.println("MPU6050 NOT FOUND - Running in manual mode");
+  } else {
+    Serial.println("MPU6050 OK!");
+  }
 }
 
 void loop() {
-  // Пусто: машинка будет ехать постоянно с заданной скоростью
-}
+  // Тестовый цикл: 1 секунда вперед, 1 секунда назад
+  Serial.println("Forward...");
+  digitalWrite(IN1, HIGH); digitalWrite(IN2, LOW);
+  digitalWrite(IN3, HIGH); digitalWrite(IN4, LOW);
+  analogWrite(ENA, 255); analogWrite(ENB, 255);
+  delay(500);
 
-void moveForward(int speed) {
-  // Направление для левого мотора
-  digitalWrite(IN1, HIGH);
-  digitalWrite(IN2, LOW);
-  analogWrite(ENA, speed); // Регулировка напряжения через ШИМ
-
-  // Направление для правого мотора
-  digitalWrite(IN3, HIGH);
-  digitalWrite(IN4, LOW);
-  analogWrite(ENB, speed); // Регулировка напряжения через ШИМ
+  Serial.println("Backward...");
+  digitalWrite(IN1, LOW); digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, LOW); digitalWrite(IN4, HIGH);
+  delay(500);
 }
